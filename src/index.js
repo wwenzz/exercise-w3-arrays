8c8529251e61d3667465043052c63a1eafbc4d16
Wendy Zhao <wendy.w.zhao@outlook.com>
2024-02-20 14:22:45 +0100

    finish the exercise in group

--- src/index.js
+++ src/index.js
@@ -11,54 +11,100 @@ const pokemons = [
   "Metapod",
   "Weedle",
   "Pikachu",
-  "Pidgey"
+  "Pidgey",
 ];
 
 const countThem = () => {
   // print out, in a sentence, how many pokemons I have.
   // like: "I have x pokemons!"
+  console.log(`I have ${pokemons.length} pokemons!`);
 };
+countThem();
 
 const orderThem = () => {
   // order the pokemons alphabetically
+  console.log(pokemons.sort());
 };
+orderThem();
 
 const flipThem = () => {
   // reverse the order of the pokemons
+  console.log(pokemons.reverse());
 };
+flipThem();
 
 const makeThemBig = () => {
   // print the pokemons in UPPERCASE letters
+
+  pokemons.forEach((pokemon) => console.log(pokemon.toUpperCase()));
 };
+makeThemBig();
 
 const onlyTheBs = () => {
-  // only print the pokemons that starts with B
+  console.log(
+    pokemons.filter((pokemon) => {
+      return pokemon[0] === "B";
+    })
+  );
 };
+onlyTheBs();
 
 const notTheCs = () => {
   // remove all pokemons that starts with C
+  console.log(
+    pokemons.filter((pokemon) => {
+      return pokemon[0] !== "C";
+    })
+  );
 };
+notTheCs();
+console.log("Before Splice:\n", pokemons);
+
+const removeTheCs = () => {
+  pokemons.splice(8, 3);
+};
+
+removeTheCs();
+console.log("After Splice:\n", pokemons);
 
 const nameAndIdThanks = () => {
   // print out name and index of all pokemons
   // like: number x - Squirtle
+  pokemons.forEach((pokemon, i) => {
+    console.log(`${i} - ${pokemon}`);
+  });
 };
 
-const catchPokemon = name => {
+nameAndIdThanks();
+
+const catchPokemon = (name) => {
   // add a pokemon with a name of your choice to the list,
   // print the list so you see its there.
+  pokemons.push("Charizard");
+  console.log(pokemons);
 };
+catchPokemon();
 
-const didICatchIt = name => {
+const didICatchIt = (name) => {
   // check the pokemons to see if a specific pokemon is in the array
+  console.log(pokemons.includes("Charizard"));
 };
+didICatchIt();
 
 const addInThirdPlace = () => {
   // add the pokemon "Clefairy" in the third place of the array
   // print the list so you see its there.
+  pokemons.splice(2, 0, "Clefairy");
+  console.log(pokemons);
 };
+addInThirdPlace();
 
 // ***BONUS***
 const theLongestName = () => {
   // find the pokemon with the longest name
+  console.log(pokemons.sort((a, b) => b.length - a.length));
+  console.log(pokemons.sort((a, b) => a.length - b.length)[11]);
+  console.log(pokemons.sort((a, b) => b.length - a.length)[0]);
+  //b-a
 };
+theLongestName();
